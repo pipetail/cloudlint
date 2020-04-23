711a60c8fd10c4f0098e8e0333482fc247291851
Stepan Vrany <stepan@vrany.dev>
2020-04-23 11:17:03 +0000

    mock GetCostAndUsage for costexplorer

--- internal/app/worker/check_integration.go
+++ internal/app/worker/check_integration.go
@@ -1,6 +1,10 @@
 package worker
 
 import (
+	"fmt"
+	"log"
+
+	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/aws/session"
 	"github.com/aws/aws-sdk-go/service/costexplorer"
 	"github.com/aws/aws-sdk-go/service/costexplorer/costexploreriface"
@@ -17,6 +21,25 @@ func checkIntegration() error {
 	return nil
 }
 
-func checkIntegrationHandler(costExplorerScv costexploreriface.CostExplorerAPI) (string, error) {
-	return "bla", nil
+func checkIntegrationHandler(costExplorerScv costexploreriface.CostExplorerAPI) (*string, error) {
+
+	costParams := &costexplorer.GetCostAndUsageInput{
+		Granularity: aws.String("MONTHLY"),
+		Metrics:     []*string{aws.String("UnblendedCost")},
+		TimePeriod: &costexplorer.DateInterval{
+			Start: aws.String("2019-09-01"),
+			End:   aws.String("2019-11-01"),
+		},
+	}
+
+	result, err := costExplorerScv.GetCostAndUsage(costParams)
+	if err != nil {
+		return nil, fmt.Errorf("GetCostAndUsage: %s", err)
+	}
+
+	log.Println(result)
+
+	test := "bla"
+
+	return &test, nil
 }
--- internal/app/worker/check_integration_test.go
+++ internal/app/worker/check_integration_test.go
@@ -1,13 +1,60 @@
 package worker
 
 import (
+	"encoding/json"
+	"fmt"
 	"testing"
 
+	"github.com/aws/aws-sdk-go/service/costexplorer"
 	"github.com/aws/aws-sdk-go/service/costexplorer/costexploreriface"
 )
 
 type CostExplorerMockClient struct {
 	costexploreriface.CostExplorerAPI
+}
+
+func (client *CostExplorerMockClient) GetCostAndUsage(input *costexplorer.GetCostAndUsageInput) (*costexplorer.GetCostAndUsageOutput, error) {
+
+	payload := `{
+		"ResultsByTime": [
+			{
+				"TimePeriod": {
+					"Start": "2019-09-01",
+					"End": "2019-10-01"
+				},
+				"Total": {
+					"UnblendedCost": {
+						"Amount": "0.0000030803",
+						"Unit": "USD"
+					}
+				},
+				"Groups": [],
+				"Estimated": false
+			},
+			{
+				"TimePeriod": {
+					"Start": "2019-10-01",
+					"End": "2019-11-01"
+				},
+				"Total": {
+					"UnblendedCost": {
+						"Amount": "7782.9981148686",
+						"Unit": "USD"
+					}
+				},
+				"Groups": [],
+				"Estimated": false
+			}
+		]
+	}`
+
+	output := costexplorer.GetCostAndUsageOutput{}
+	err := json.Unmarshal([]byte(payload), &output)
+	if err != nil {
+		return nil, fmt.Errorf("json.Unmarshal: %s", err)
+	}
+
+	return &output, nil
 }
 
 func TestCheckIntegrationHandler(t *testing.T) {

ec6748e9208d0734cca46549c951d85fd957e7aa
Stepan Vrany <stepan@vrany.dev>
2020-04-23 14:16:33 +0000

    add debug mode

--- cmd/cloudlint/main.go
+++ cmd/cloudlint/main.go
@@ -2,11 +2,19 @@ package main
 
 import (
 	"github.com/pipetail/cloudlint/internal/app/worker"
+	log "github.com/sirupsen/logrus"
 	"github.com/spf13/cobra"
 )
 
 var (
-	cfgFile string
+	cfgFile       string   // path of cnfiguration file
+	analizeChecks []string // list of checks we want to execute
+	debug         bool     // indicates verbose output
+
+	// default values
+	analizeChecksDefault = []string{"jedna", "dva"}
+
+	// commands
 	rootCmd = &cobra.Command{
 		Use:   "cloudlint",
 		Short: "", // add some clever but short description
@@ -26,9 +34,12 @@ func init() {
 
 	// root command
 	rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.config/cloudlint.yaml)")
+	rootCmd.PersistentFlags().StringArrayVar(&analizeChecks, "checks", analizeChecksDefault, "list of checks you want to run agains infrastructure")
+	rootCmd.PersistentFlags().BoolVar(&debug, "debug", false, "verbose output")
 
 	// analyze command
 	rootCmd.AddCommand(analyzeCmd)
+
 }
 
 func main() {
@@ -39,5 +50,10 @@ func initConfig() {
 }
 
 func analyze(cmd *cobra.Command, args []string) {
+	if debug {
+		log.SetLevel(log.DebugLevel)
+	}
+
+	log.WithField("checks", analizeChecks).Debug("received list of checks")
 	worker.Handle()
 }

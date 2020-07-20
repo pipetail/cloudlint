a763e15ec47fce94790f1f9f0c5dd0a5bad43097
Daniel Lamper <daniel.lamper@hotmail.com>
2020-07-20 11:37:01 +0200

    Add conversion of volume type from shortcut to full name

--- pkg/awspricing/awspricing.go
+++ pkg/awspricing/awspricing.go
@@ -166,7 +166,7 @@ func GetPriceOfVolume(client pricingiface.PricingAPI, volumeType string, region 
         {
             Field: aws.String("volumeType"),
             Type:  aws.String("TERM_MATCH"),
-            Value: aws.String(volumeType),
+            Value: aws.String(utils.ConvertVolumeTypeForPricing(volumeType)),
         },
     }
 
